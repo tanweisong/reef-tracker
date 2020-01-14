0eb111440e8e99bc8cc4e535f04a6ffc90a456fa
BeckTanWeiSong <beck.tan@inmindcloud.com>
2020-01-14 11:06:09 +0800

    sort setting via brand

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -11,6 +11,25 @@ export default {
         calcium: [],
         magnesium: []
       };
+    else {
+      _.forOwn(payload, function(value, key, obj) {
+        switch (key) {
+          case 'alkalinity':
+          case 'calcium':
+          case 'magnesium':
+            value = value.sort(function(a, b) {
+              const brandA = _.get(a, 'brand');
+              const brandB = _.get(b, 'brand');
+
+              return brandA < brandB ? -1 : 1;
+            });
+            obj = value;
+            break;
+          default:
+            break;
+        }
+      });
+    }
 
     context.commit('setSettings', payload);
   },
